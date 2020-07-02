019bb39b5b55ca3baf39a331a70d4d84cedec681
Roig <groig@protonmail.com>
2020-07-02 17:33:51 -0400

    quick actions POC

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,8 @@ import 'package:todo/pages/settings_page.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/services/AppStateNotifier.dart';
 import 'package:todo/pages/account_page.dart';
+import 'package:quick_actions/quick_actions.dart';
+import 'package:todo/services/phone.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -52,6 +54,37 @@ class _HomePageState extends State<HomePage> {
         });
       }
     });
+
+    final QuickActions quickActions = QuickActions();
+
+    quickActions.initialize((String shortcutType) {
+      switch (shortcutType) {
+        case 'saldo':
+          callTo('*222#');
+          break;
+        case 'datos':
+          callTo('*222*328#');
+          break;
+        case 'bono':
+          callTo('*222*266#');
+          break;
+      }
+    });
+
+    quickActions.setShortcutItems(<ShortcutItem>[
+      const ShortcutItem(
+        type: 'saldo',
+        localizedTitle: 'Consultar saldo',
+        icon: 'attach_money',
+      ),
+      const ShortcutItem(
+        type: 'datos',
+        localizedTitle: 'Consultar datos',
+        icon: 'data_ussage',
+      ),
+      const ShortcutItem(
+          type: 'bono', localizedTitle: 'Consultar bono', icon: 'data_ussage'),
+    ]);
   }
 
   @override
